4847c7245846df50348d4ccd495f7ad53ea3ed54
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-10-02 21:28:47 +0800

    👆Added index.css for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-    important: true,
-    purge: [],
-    theme: {
-      extend: {},
-      container: {
-        center: true,
-      },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        xxl: "1560px",
-      },
-      extend: {
-        colors: {
-          dark: "#0B2545",
-          accent: {
-            light: "#8DA9C4",
-            dark: "#13315C",
-            mid: "#134074",
-          },
-          foreground: "#EEF4ED",
-        }
-      },
-  
+  important: true,
+  purge: [],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
     },
-    variants: {
-      animation: ['responsive', 'hover', 'focus'],
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1560px",
     },
-    plugins: [],
-    future: {
-      removeDeprecatedGapUtilities: true,
+    extend: {
+      colors: {
+        dark: "#0B2545",
+        accent: {
+          light: "#8DA9C4",
+          dark: "#13315C",
+          mid: "#134074",
+        },
+        foreground: "#EEF4ED",
+      }
     },
-  };
+
+  },
+  variants: {
+    animation: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
