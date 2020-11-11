14e51e2942fe2611e28b7833ddee3f2e8c7ae906
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-11-11 22:20:37 +0800

    🔨Fixed responsive footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,19 +3,19 @@ import FooterLink from "./footerLink";
 export default function Footer() {
   return (
     <div className="bg-dark w-full ">
-      <div className="container py-16">
-        <div className="container text-foreground flex flex-row flex-no-wrap justify-between mx-auto w-11/12 md:w-10/12 lg:w-9/12">
-          <div>
-            <FooterLink href="blog.ranegillian.me" text="blog.ranegillian.me" />
+      <div className="container pt-8 pb-16">
+        <div className="container text-foreground flex flex-col flex-no-wrap lg:justify-between lg:flex-row mx-auto w-11/12 md:w-10/12 lg:w-9/12">
+          <div className="mx-auto lg:mx-0">
+            <FooterLink href="https://blog.ranegillian.me/" text="blog.ranegillian.me" />
             &nbsp;&copy;&nbsp;2020
           </div>
-          <div>
+          <div className="mx-auto lg:mx-0">
             Created and Powered by&nbsp;
             <FooterLink href="https://nextjs.org/" text="Next.JS" />
             &nbsp;and&nbsp;
             <FooterLink href="https://strapi.io/" text="Strapi" />
           </div>
-          <div>
+          <div className="mx-auto lg:mx-0">
             <FooterLink href="https://www.ranegillian.me/" text="Website" />
             &nbsp;&bull;&nbsp;
             <FooterLink
